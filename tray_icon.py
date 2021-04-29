3a952c6b693d875b4e7642c2f5c991620a3290c1
habibulilalbaab <intprismtechnology@gmail.com>
2021-04-29 23:14:24 +0700

    Update tray_icon.py
    fix error when failed get data from api & add last time update

--- tray_icon.py
+++ tray_icon.py
@@ -1,28 +1,28 @@
 from infi.systray import SysTrayIcon
 import time
 import requests
+import datetime
 
-with SysTrayIcon("assets/level-2.ico", "") as systray:
+with SysTrayIcon("assets/level-1.ico", "") as systray:
     while True:
-        ip_address = "192.168.5.5"
-
-        battery_percentage = "http://{}/api/percentage".format(ip_address)
-        battery_percentage = requests.get(battery_percentage)
-        battery_percentage = battery_percentage.text
-
-        battery_voltage = "http://{}/api/voltage".format(ip_address)
-        battery_voltage = requests.get(battery_voltage)
-        battery_voltage = battery_voltage.text
-
-        battery_status = "http://{}/api/status".format(ip_address)
-        battery_status = requests.get(battery_status)
-        battery_status = battery_status.text
-
-        battery_level = "http://{}/api/level".format(ip_address)
-        battery_level = requests.get(battery_level)
-        battery_level = battery_level.text
-
-        battery_data = "{}% Battery Percentage\n{}V Battery Voltage\nStatus is {}".format(battery_percentage, battery_voltage, battery_status)
+        try:
+            ip_address = "192.168.5.5"
+            battery_percentage = "http://{}/api/percentage".format(ip_address)
+            battery_voltage = "http://{}/api/voltage".format(ip_address)
+            battery_status = "http://{}/api/status".format(ip_address)
+            battery_level = "http://{}/api/level".format(ip_address)
+            battery_percentage = requests.get(battery_percentage, timeout=3)
+            battery_voltage = requests.get(battery_voltage, timeout=3)
+            battery_status = requests.get(battery_status, timeout=3)
+            battery_level = requests.get(battery_level, timeout=3)
+            battery_percentage = battery_percentage.text
+            battery_voltage = battery_voltage.text
+            battery_status = battery_status.text
+            battery_level = battery_level.text
+            print("[200] Success Get Data")
+        except:
+            print("[500] Failed Get Data")
+        battery_data = "{}% Battery Percentage\n{}V Battery Voltage\nStatus is {}\nUpdated at {}".format(battery_percentage, battery_voltage, battery_status, datetime.datetime.now().strftime("%d-%m-%Y %H:%I:%S"))
         if battery_level == "1":
             systray.update(icon="assets/level-1.ico", hover_text=battery_data)
         elif battery_level == "2":
